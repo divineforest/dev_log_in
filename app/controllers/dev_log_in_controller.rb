1a30bbe5c9d660916a0301812b412269583bb591
Alexander Balashov <divineforest@gmail.com>
2011-10-06 18:11:32 +0400

    Work in test env also (for quick integration testing)

--- app/controllers/dev_log_in_controller.rb
+++ app/controllers/dev_log_in_controller.rb
@@ -1,11 +1,11 @@
 class DevLogInController < ApplicationController
 
   def log_in
-    if Rails.env.development?
+    if Rails.env.development? || Rails.env.test?
       user = User.where(["email LIKE ?", params[:login]+'%']).first || (raise "Can't find user '#{params[:login]}'")
       sign_in_and_redirect(user)
     else
-      raise "Works only development env"
+      raise "Works only in development or test env"
     end
   end
 
