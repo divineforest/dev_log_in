e16da353765fbc84992fe6a7aa25d3907a572e47
Dmitry Vorotilin <d.vorotilin@gmail.com>
2012-07-17 13:43:12 +0400

    Explicit search by username in email string

--- app/controllers/dev_log_in_controller.rb
+++ app/controllers/dev_log_in_controller.rb
@@ -2,7 +2,7 @@ class DevLogInController < ApplicationController
 
   def log_in
     if Rails.env.development? || Rails.env.test?
-      user = User.where(["email LIKE ?", params[:login]+'%']).first || (raise "Can't find user '#{params[:login]}'")
+      user = User.where(User.arel_table[:email].matches("#{params[:login]}@%")).first!
       sign_in_and_redirect(user)
     else
       raise "Works only in development or test env"
