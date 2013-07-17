999ec84f24d14cdc63dde7fd305decd4450d7dd3
Alexander Balashov <divineforest@gmail.com>
2013-07-17 12:18:31 +0400

    More simple redirect logic.

--- app/controllers/dev_log_in_controller.rb
+++ app/controllers/dev_log_in_controller.rb
@@ -4,7 +4,9 @@ class DevLogInController < ActionController::Base
     if Rails.env.development? || Rails.env.test?
       model_class = params[:model].present? ? params[:model].classify.constantize : User
       user = model_class.where(model_class.arel_table[:email].matches("#{params[:login]}@%")).first!
-      sign_in_and_redirect(user)
+
+      sign_in(user)
+      redirect_to "/"
     else
       raise "Works only in development or test env"
     end
