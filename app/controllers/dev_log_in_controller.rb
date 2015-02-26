5537111b7b06efa1cc0d5df963b544d630594413
Kir Shatrov <shatrov@me.com>
2015-02-26 21:00:03 +0200

    Log in with id

--- app/controllers/dev_log_in_controller.rb
+++ app/controllers/dev_log_in_controller.rb
@@ -3,13 +3,24 @@ class DevLogInController < ActionController::Base
   def log_in
     if Rails.env.development? || Rails.env.test?
       model_class = params[:model].present? ? params[:model].classify.constantize : User
-      user = model_class.where(model_class.arel_table[:email].matches("#{params[:login]}@%")).first!
+
+      user = if is_numeric?(params[:login])
+        model_class.find(params[:login])
+      else
+        model_class.where(model_class.arel_table[:email].matches("#{params[:login]}@%")).first!
+      end
 
       sign_in(user)
       redirect_to "/"
     else
       raise "Works only in development or test env"
     end
+  end
+
+  private
+
+  def is_numeric?(id)
+    id =~ /\A\d+\z/ ? true : false
   end
 
 end
